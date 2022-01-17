9b43c0cea0ba4d7fc62d0ed2facc37ad665cc2e1
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-01-17 17:24:15 +0300

    Move to time-compat

--- library/Data/Hashable/Time.hs
+++
@@ -1,7 +0,0 @@
-module Data.Hashable.Time
-(
-  module Rebase.Data.Hashable.Time
-)
-where
-
-import Rebase.Data.Hashable.Time
---
+++ library/Data/Time/Compat.hs
@@ -0,0 +1,7 @@
+module Data.Time.Compat
+(
+  module Rebase.Data.Time.Compat
+)
+where
+
+import Rebase.Data.Time.Compat
